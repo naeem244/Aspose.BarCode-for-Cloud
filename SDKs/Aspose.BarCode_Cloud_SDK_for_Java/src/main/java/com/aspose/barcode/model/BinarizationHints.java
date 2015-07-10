d40cef853b305a7c6298e7b5d23e3d74bab03811
Farooq Sheikh <farooq.sheikh@aspose.com>
2015-07-10 15:32:46 +0500

    Aspose.BarCode Cloud SDK For Java - Version 1.0.0

--- SDKs/Aspose.BarCode_Cloud_SDK_for_Java/src/main/java/com/aspose/barcode/model/BinarizationHints.java
+++ SDKs/Aspose.BarCode_Cloud_SDK_for_Java/src/main/java/com/aspose/barcode/model/BinarizationHints.java
@@ -1,12 +1,26 @@
 package com.aspose.barcode.model;
 
-public class BinarizationHints {
-  @Override
-  public String toString()  {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class BinarizationHints {\n");
-    sb.append("}\n");
-    return sb.toString();
-  }
+public enum  BinarizationHints {
+   None("None"),
+   Grayscale("Grayscale"),
+   MedianSmoothing("MedianSmoothing"),
+   InvertImage("InvertImage"),
+   ComplexBackground("ComplexBackground");
+      
+      private final String binarizationHintsValue;
+      
+      private BinarizationHints(final String binarizationHintsValue) {
+          this.binarizationHintsValue = binarizationHintsValue;
+      }
+    
+      /* (non-Javadoc)
+       * @see java.lang.Enum#toString()
+       */
+      @Override
+      public String toString() {
+          return binarizationHintsValue;
+      }
+   
+   
 }
 
